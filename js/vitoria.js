4a3c93954644d8c292faf25a33d390689c0b01f1
Julia Gama <gamaj235@gmail.com>
2021-08-10 16:51:21 -0300

    faltando algumas partes da vitoria

--- js/vitoria.js
+++ js/vitoria.js
@@ -1,11 +1,11 @@
-let red  = ["l0_0", "l0_8", "l0_2", "l0_3"];
+let red  = ["l0_0", "l0_8", "l0_2", "l0_3", "l0_5"];
 let blue = ["l1_0", "l1_1", "l1_2", "l1_3"];
 
-let discoDaVez = "l1_4";
+let discoDaVez = "l0_4";
 let contrutorPosicao = discoDaVez.substring(0,3)
-let corDaVez   = "blue";
+let corDaVez   = "red";
 
-function verificarHorizontal() {
+function verificarHorizontalMenos() {
   let contador = 0;
   let discoComparando = Number(discoDaVez[3])-1
   let comparacao = `${contrutorPosicao}${discoComparando}`;
@@ -16,7 +16,7 @@ function verificarHorizontal() {
       if (red.includes(comparacao)) {
         contador++;
       } else {
-        return "antes não tem";
+        compararHorizontalMais();
       }
       discoComparando = Number(comparacao[3])-1;
       comparacao = `${contrutorPosicao}${discoComparando}`;
@@ -26,9 +26,39 @@ function verificarHorizontal() {
       if (blue.includes(comparacao)) {
         contador++;
       } else {
-        return "antes não tem";
+        compararHorizontalMais();
       }
       discoComparando = Number(comparacao[3])-1;
+      comparacao = `${contrutorPosicao}${discoComparando}`;
+    }
+  }
+  return contador;
+}
+
+function compararHorizontalMais() {
+  let contador = 0;
+  let discoComparando = Number(discoDaVez[3])+1
+  let comparacao = `${contrutorPosicao}${discoComparando}`;
+  console.log(comparacao)
+
+  if (corDaVez === "red") {
+    for (let i = 0; i < red.length; i++) {
+      if (red.includes(comparacao)) {
+        contador++;
+      } else {
+        return "antes não tem";
+      }
+      discoComparando = Number(comparacao[3])+1;
+      comparacao = `${contrutorPosicao}${discoComparando}`;
+    }
+  } else {
+    for (let i = 0; i < blue.length; i++) {
+      if (blue.includes(comparacao)) {
+        contador++;
+      } else {
+        return "antes não tem";
+      }
+      discoComparando = Number(comparacao[3])+1;
       comparacao = `${contrutorPosicao}${discoComparando}`;
     }
   }
