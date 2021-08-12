ed56cc6d29a1a5907fe24db335b0cd22fb2e9600
jpnonato <jpnonatox@gmail.com>
2021-08-11 22:23:05 -0300

    ajuste modal

--- js/jogadorVenceu.js
+++ js/jogadorVenceu.js
@@ -1,4 +1,4 @@
-function animacao() {
+function victoryPlayer1() {
     let res = document.getElementById('modal')
     res.style.display = 'block'
     }
@@ -8,4 +8,15 @@ function animacao() {
     botao.addEventListener('click', function (event){
     window.location.reload()
     })
+
+function victoryPlayer2() {
+        let res2 = document.getElementById('modal2')
+        res2.style.display = 'block'
+        }
+        
+        let botao2 = document.getElementById('reset2')
+        
+        botao2.addEventListener('click', function (event){
+        window.location.reload()
+        })
     
